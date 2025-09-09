565ccd54e865cd8589f9f3c98315bb63a1b8cb86
Ali Razi <95105244+alirazi1992@users.noreply.github.com>
2025-09-09 13:03:30 +0330

    Add files via upload

---
+++ NotesRichTextGUI/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace NotesRichTextGUI
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnNew;
+        private System.Windows.Forms.ToolStripButton btnOpen;
+        private System.Windows.Forms.ToolStripButton btnSave;
+        private System.Windows.Forms.ToolStripButton btnSaveAs;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnBold;
+        private System.Windows.Forms.ToolStripButton btnItalic;
+        private System.Windows.Forms.ToolStripButton btnUnderline;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripComboBox comboFontSize;
+        private System.Windows.Forms.ToolStripButton btnColor;
+        private System.Windows.Forms.ToolStripButton btnBullet;
+        private System.Windows.Forms.ToolStripButton btnClearFormatting;
+
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnNew = new System.Windows.Forms.ToolStripButton();
+            this.btnOpen = new System.Windows.Forms.ToolStripButton();
+            this.btnSave = new System.Windows.Forms.ToolStripButton();
+            this.btnSaveAs = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnBold = new System.Windows.Forms.ToolStripButton();
+            this.btnItalic = new System.Windows.Forms.ToolStripButton();
+            this.btnUnderline = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.comboFontSize = new System.Windows.Forms.ToolStripComboBox();
+            this.btnColor = new System.Windows.Forms.ToolStripButton();
+            this.btnBullet = new System.Windows.Forms.ToolStripButton();
+            this.btnClearFormatting = new System.Windows.Forms.ToolStripButton();
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNew,
+            this.btnOpen,
+            this.btnSave,
+            this.btnSaveAs,
+            this.toolStripSeparator1,
+            this.btnBold,
+            this.btnItalic,
+            this.btnUnderline,
+            this.toolStripSeparator2,
+            this.comboFontSize,
+            this.btnColor,
+            this.btnBullet,
+            this.btnClearFormatting});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 1, 2);
+            this.toolStrip1.Size = new System.Drawing.Size(784, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnNew
+            // 
+            this.btnNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(34, 20);
+            this.btnNew.Text = "New";
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(40, 20);
+            this.btnOpen.Text = "Open";
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(35, 20);
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(53, 20);
+            this.btnSaveAs.Text = "Save As";
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 23);
+            // 
+            // btnBold
+            // 
+            this.btnBold.CheckOnClick = true;
+            this.btnBold.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnBold.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(23, 20);
+            this.btnBold.Text = "B";
+            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.CheckOnClick = true;
+            this.btnItalic.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnItalic.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(17, 20);
+            this.btnItalic.Text = "I";
+            this.btnItalic.Click += new System.EventHandler(this.btnItalic_Click);
+            // 
+            // btnUnderline
+            // 
+            this.btnUnderline.CheckOnClick = true;
+            this.btnUnderline.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnUnderline.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline);
+            this.btnUnderline.Name = "btnUnderline";
+            this.btnUnderline.Size = new System.Drawing.Size(21, 20);
+            this.btnUnderline.Text = "U";
+            this.btnUnderline.Click += new System.EventHandler(this.btnUnderline_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 23);
+            // 
+            // comboFontSize
+            // 
+            this.comboFontSize.Name = "comboFontSize";
+            this.comboFontSize.Size = new System.Drawing.Size(75, 23);
+            this.comboFontSize.ToolTipText = "Font Size";
+            this.comboFontSize.SelectedIndexChanged += new System.EventHandler(this.comboFontSize_SelectedIndexChanged);
+            // 
+            // btnColor
+            // 
+            this.btnColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(39, 20);
+            this.btnColor.Text = "Color";
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnBullet
+            // 
+            this.btnBullet.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnBullet.Name = "btnBullet";
+            this.btnBullet.Size = new System.Drawing.Size(40, 20);
+            this.btnBullet.Text = "Bullets";
+            this.btnBullet.Click += new System.EventHandler(this.btnBullet_Click);
+            // 
+            // btnClearFormatting
+            // 
+            this.btnClearFormatting.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnClearFormatting.Name = "btnClearFormatting";
+            this.btnClearFormatting.Size = new System.Drawing.Size(98, 20);
+            this.btnClearFormatting.Text = "Clear Formatting";
+            this.btnClearFormatting.Click += new System.EventHandler(this.btnClearFormatting_Click);
+            // 
+            // rtb
+            // 
+            this.rtb.AcceptsTab = true;
+            this.rtb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtb.HideSelection = false;
+            this.rtb.Location = new System.Drawing.Point(0, 27);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(784, 514);
+            this.rtb.TabIndex = 1;
+            this.rtb.Text = "";
+            this.rtb.TextChanged += new System.EventHandler(this.rtb_TextChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 519);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(66, 17);
+            this.lblStatus.Text = "Chars: 0";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = null;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 541);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.rtb);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Notes (Day 18)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ NotesRichTextGUI/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NotesRichTextGUI
+{
+    public partial class Form1 : Form
+    {
+        private string _currentPath = null;   // full path of current doc (null = new/unsaved)
+        private bool _dirty = false;          // unsaved changes?
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Populate font sizes
+            comboFontSize.Items.AddRange(new object[]
+            {
+                "8","9","10","11","12","14","16","18","20","24","28","32"
+            });
+            comboFontSize.Text = "12";
+
+            rtb.Font = new Font("Segoe UI", 12f, FontStyle.Regular);
+
+            UpdateTitle();
+            UpdateStatus();
+        }
+
+        // ------------- UI EVENTS -------------
+
+        private void rtb_TextChanged(object sender, EventArgs e)
+        {
+            _dirty = true;
+            UpdateStatus();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            if (!ConfirmSaveIfDirty()) return;
+
+            rtb.Clear();
+            rtb.Font = new Font("Segoe UI", 12f, FontStyle.Regular);
+            _currentPath = null;
+            _dirty = false;
+            UpdateTitle();
+            UpdateStatus();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (!ConfirmSaveIfDirty()) return;
+
+            using (var dlg = new OpenFileDialog())
+            {
+                dlg.Filter = "Rich Text (*.rtf)|*.rtf|Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+                dlg.Title = "Open Note";
+                if (dlg.ShowDialog(this) == DialogResult.OK)
+                {
+                    LoadDocument(dlg.FileName);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveDocument(false);
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveDocument(true);
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+            ToggleStyle(FontStyle.Bold);
+            btnBold.Checked = IsStyleActive(FontStyle.Bold);
+        }
+
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+            ToggleStyle(FontStyle.Italic);
+            btnItalic.Checked = IsStyleActive(FontStyle.Italic);
+        }
+
+        private void btnUnderline_Click(object sender, EventArgs e)
+        {
+            ToggleStyle(FontStyle.Underline);
+            btnUnderline.Checked = IsStyleActive(FontStyle.Underline);
+        }
+
+        private void comboFontSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int size;
+            if (int.TryParse(Convert.ToString(comboFontSize.SelectedItem), out size))
+            {
+                ApplyFontSize(size);
+            }
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new ColorDialog())
+            {
+                dlg.FullOpen = true;
+                dlg.Color = rtb.SelectionColor;
+                if (dlg.ShowDialog(this) == DialogResult.OK)
+                {
+                    rtb.SelectionColor = dlg.Color;
+                    _dirty = true;
+                }
+            }
+        }
+
+        private void btnBullet_Click(object sender, EventArgs e)
+        {
+            rtb.SelectionBullet = !rtb.SelectionBullet;
+            rtb.Focus();
+            _dirty = true;
+        }
+
+        private void btnClearFormatting_Click(object sender, EventArgs e)
+        {
+            var def = rtb.Font;
+            if (def == null) def = new Font("Segoe UI", 12f, FontStyle.Regular);
+
+            rtb.SelectionFont = new Font(def, FontStyle.Regular);
+            rtb.SelectionColor = Color.Black;
+            rtb.SelectionBullet = false;
+            _dirty = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!ConfirmSaveIfDirty())
+            {
+                e.Cancel = true;
+            }
+        }
+
+        // ------------- HELPERS -------------
+
+        private void UpdateTitle()
+        {
+            var name = _currentPath == null ? "Untitled" : Path.GetFileName(_currentPath);
+            this.Text = (_dirty ? "*" : "") + name + " - Notes (Day 18)";
+        }
+
+        private void UpdateStatus()
+        {
+            int chars = rtb.TextLength;
+            lblStatus.Text = "Chars: " + chars + (_dirty ? " • Unsaved" : "");
+            UpdateTitle();
+        }
+
+        private bool ConfirmSaveIfDirty()
+        {
+            if (!_dirty) return true;
+
+            var result = MessageBox.Show(
+                "You have unsaved changes. Save now?",
+                "Unsaved Changes",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Cancel) return false;
+            if (result == DialogResult.Yes) return SaveDocument(false);
+            return true; // No
+        }
+
+        private void LoadDocument(string path)
+        {
+            try
+            {
+                string ext = Path.GetExtension(path).ToLowerInvariant();
+                if (ext == ".rtf")
+                {
+                    rtb.LoadFile(path, RichTextBoxStreamType.RichText);
+                }
+                else
+                {
+                    rtb.Text = File.ReadAllText(path);
+                }
+
+                _currentPath = path;
+                _dirty = false;
+                UpdateTitle();
+                UpdateStatus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open file: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool SaveDocument(bool saveAs)
+        {
+            try
+            {
+                string path = _currentPath;
+
+                if (saveAs || string.IsNullOrEmpty(path))
+                {
+                    using (var dlg = new SaveFileDialog())
+                    {
+                        dlg.Filter = "Rich Text (*.rtf)|*.rtf|Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+                        dlg.Title = "Save Note";
+                        if (!string.IsNullOrEmpty(_currentPath))
+                            dlg.FileName = Path.GetFileName(_currentPath);
+                        if (dlg.ShowDialog(this) != DialogResult.OK) return false;
+                        path = dlg.FileName;
+                    }
+                }
+
+                string ext = Path.GetExtension(path).ToLowerInvariant();
+                if (ext == ".rtf")
+                {
+                    rtb.SaveFile(path, RichTextBoxStreamType.RichText);
+                }
+                else
+                {
+                    File.WriteAllText(path, rtb.Text);
+                }
+
+                _currentPath = path;
+                _dirty = false;
+                UpdateTitle();
+                UpdateStatus();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to save: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private void ToggleStyle(FontStyle style)
+        {
+            Font sel = rtb.SelectionFont;
+            if (sel == null) sel = rtb.Font; // mixed selection → use base font
+
+            FontStyle newStyle;
+
+            if ((sel.Style & style) == style)
+            {
+                // Turn OFF this style
+                newStyle = sel.Style & ~style;
+            }
+            else
+            {
+                // Turn ON this style
+                newStyle = sel.Style | style;
+            }
+
+            rtb.SelectionFont = new Font(sel, newStyle);
+            _dirty = true;
+            rtb.Focus();
+        }
+
+        private bool IsStyleActive(FontStyle style)
+        {
+            Font sel = rtb.SelectionFont;
+            if (sel == null) sel = rtb.Font;
+            return (sel.Style & style) == style;
+        }
+
+        private void ApplyFontSize(int size)
+        {
+            Font sel = rtb.SelectionFont;
+            if (sel == null) sel = rtb.Font;
+            rtb.SelectionFont = new Font(sel.FontFamily, size, sel.Style);
+            _dirty = true;
+            rtb.Focus();
+        }
+    }
+}
